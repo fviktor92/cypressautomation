8a69e5a94805f90cd3a8906ad0a019a661e7f6e8
fviktor92 <farkas.viktor92@gmail.com>
2020-07-12 10:27:50 +0200

    first test case

--- cypress/integration/examples/test1.ts
+++ cypress/integration/examples/test1.ts
@@ -1,8 +1,15 @@
 describe('My First Test Suite', function ()
 {
-	let url: string = "https://www.seleniumeasy.com/test/";
-	it('My First Test Case', function ()
-	{
-		cy.visit(url)
-	});
+    let url: string = "https://www.seleniumeasy.com/test/";
+    it('My First Test Case', function ()
+    {
+        cy.visit(url);
+        cy.get("#at-cv-lightbox-close").click();
+        cy.get("#navbar-brand-centered ul:nth-child(1) li.dropdown:nth-child(1)").click()
+          .contains("Ajax Form Submit").click()
+          .get("#title").type("Viktor Farkas")
+          .get("#description").type("This is a Cypress test")
+          .get("#btn-submit").click()
+          .get("#submit-control").should("contain.text", "Form submited Successfully!");
+    });
 });
