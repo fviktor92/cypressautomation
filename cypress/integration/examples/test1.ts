6474ade11f35200410e47e5143fd35be495740fa
fviktor92 <farkas.viktor92@gmail.com>
2020-07-16 22:06:50 +0200

    dropdowns

--- cypress/integration/examples/test1.ts
+++ cypress/integration/examples/test1.ts
@@ -1,15 +1,57 @@
-describe('My First Test Suite', function ()
+describe('My First Test Suite', function (): void
 {
-    let url: string = "https://www.seleniumeasy.com/test/";
-    it('My First Test Case', function ()
+    it("Product filtering", function (): void
     {
-        cy.visit(url);
-        cy.get("#at-cv-lightbox-close").click();
-        cy.get("#navbar-brand-centered ul:nth-child(1) li.dropdown:nth-child(1)").click()
-          .contains("Ajax Form Submit").click()
-          .get("#title").type("Viktor Farkas")
-          .get("#description").type("This is a Cypress test")
-          .get("#btn-submit").click()
-          .get("#submit-control").should("contain.text", "Form submited Successfully!");
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get(".search-keyword").type("ca")
+            // Finds the descendent DOM elements with the given selector and clicking the third
+          .get(".products").as("productsLocator").find(".product").should("have.length", 4)
+          .eq(2).contains("ADD TO CART").click();
+        // Iterating through found elements and clicking that contains expected text
+        cy.get("@productsLocator").find(".product").each((element: JQuery<HTMLElement>): void =>
+        {
+            let textVeg: string = element.find("h4.product-name").text();
+            if (textVeg.includes("Cashews"))
+            {
+                element.find("button").trigger("click");
+            }
+        })
+        // Resolving the promise ourselves and getting the text of element
+        let logo: Cypress.Chainable<JQuery<HTMLElementTagNameMap["div"]>> = cy.get(".brand");
+        logo.should("have.text", "GREENKART");
+        logo.then(function (logoElement: JQuery<HTMLElementTagNameMap["div"]>): void
+        {
+            cy.log(logoElement.text());
+        })
+    });
+
+    it("Handling Web Controls UI", function (): void
+    {
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+        // Checkbox
+        cy.get("#checkBoxOption1").check().should("be.checked").and("have.value", "option1");
+        cy.get("#checkBoxOption1").uncheck().should("not.be.checked");
+        cy.get("input[type='checkbox']").check(["option2", "option3"]);
+
+        // Static dropdown
+        cy.get("select").select("option2").should("have.value", "option2");
+
+        // Dynamic dropdown
+        cy.get("#autocomplete").type("ind");
+        cy.get(".ui-menu-item").each(element =>
+        {
+            if (element.text() === "India")
+            {
+                element.trigger("click");
+            }
+        });
+        cy.get("#autocomplete").should("have.value", "India");
+
+        // Handling visible and invisible elements using Assertions
+        cy.get("#displayed-text").should("be.visible");
+        cy.get("#hide-textbox").click();
+        cy.get("#displayed-text").should("not.be.visible");
+        cy.get("#show-textbox").click();
+        cy.get("#displayed-text").should("be.visible");
     });
 });
