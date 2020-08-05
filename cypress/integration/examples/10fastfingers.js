aaa601a0c52552e9dc8a2a91cb6139294bdae411
fviktor92 <farkas.viktor92@gmail.com>
2020-08-05 19:03:39 +0200

    10 fast

--- cypress/integration/examples/10fastfingers.js
+++ cypress/integration/examples/10fastfingers.js
@@ -2,12 +2,12 @@
 describe("10 fast fingers", function () {
     it("Hungarian", function () {
         cy.visit("https://10fastfingers.com/typing-test/hungarian");
-        cy.get("#CybotCookiebotDialogBodyLevelButtonAccept").click();
+        cy.get("#CybotCookiebotDialogBodyLevelButtonAccept").click({ force: true });
         let words = "";
         cy.get("#words span").each((word) => {
             words = words.concat(word.text()).concat(" ");
         }).then(() => {
-            cy.get("#inputfield").type(words);
+            cy.get("#inputfield").type(words, { force: true });
         });
     });
 });
